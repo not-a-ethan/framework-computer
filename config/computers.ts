af254d1630bab8856afc45e3e9824d6408bf7c39
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-11-21 20:46:51 -0500

    Added Info about the 3 laptops

---
+++ config/computers.ts
@@ -0,0 +1,153 @@
+import { Framework12, Framework13, Framework16, FrameworkDesktop, Computers } from "@/types";
+
+const framework12: Framework12 = {
+    "ram": {
+        "slots": 1,
+        "maxTotal": 48,
+        "maxPerSlot": 48
+    },
+    "storage": {
+        "slots": 1,
+        "sizes": [2230]
+    },
+    "cpu": {
+        "i3-1315U": {
+            "clockSpeed": 4.5,
+            "cores": 2,
+            "threads": 4
+        },
+        "i5-1334U": {
+            "clockSpeed": 4.6,
+            "cores": 2,
+            "threads": 8
+        }
+    },
+    "power": 60
+};
+
+const framework13: Framework13 = {
+    "ram": {
+        "slots": 2,
+        "maxTotal": 96,
+        "maxPerSlot": 48
+    },
+    "storage": {
+        "slots": 1,
+        "sizes": [2280]
+    },
+    "old builds": {
+        "amd": {
+            "AMD Ryzen™ 5 7640U": {
+                "clockSpeed": 4.9,
+                "cores": 6,
+                "threads": 12,
+                "battery": 61,
+                "webcamGen": 2,
+                "displayInfo": '13.5" 2256x1504 60Hz 2.2K matte display'
+            },
+            "AMD Ryzen™ 5 7640U (2.8k Display)": {
+                "clockSpeed": 4.9,
+                "cores": 6,
+                "threads": 12,
+                "battery": 61,
+                "webcamGen": 2,
+                "displayInfo": '13.5" 2880x1920 120Hz 2.8K matte display '
+            }
+        }
+    },
+    "new builds": {
+        "cpu": {
+            "amd": {
+                "Ryzen™ AI 5 340": {
+                    "clockSpeed": 4.8,
+                    "cores": 6,
+                    "threads": 12
+                },
+                "Ryzen™ AI 7 350": {
+                    "clockSpeed": 5,
+                    "cores": 8,
+                    "threads": 16
+                },
+                "Ryzen™ AI 9 HX 370": {
+                    "clockSpeed": 5.1,
+                    "cores": 12,
+                    "threads": 24
+                }
+            },
+            "intel": {
+                "Ultra 5 125H": {
+                    "clockSpeed": 4.5,
+                    "cores": 4,
+                    "threads": 8
+                },
+                "Ultra 7 155H": {
+                    "clockSpeed": 4.8,
+                    "cores": 6,
+                    "threads": 8
+                },
+                "Ultra 7 165H": {
+                    "clockSpeed": 5,
+                    "cores": 6,
+                    "threads": 12
+                }
+            }
+        },
+        "display": {
+            "2.2K Display": '13.5" 2256 x 1504 60Hz 2.2K matte display',
+            "2.8K Display (rounded)": '13.5" 2880x1920 120Hz matte display'
+        }
+    },
+    "power": 60
+};
+
+const framework16: Framework16 = {
+    "ram": {
+        "slots": 2,
+        "maxTotal": 96,
+        "maxPerSlot": 48
+    },
+    "storage": {
+        "slots": 2,
+        "sizes": [2280, 2230]
+    },
+    "cpu": {
+        "Ryzen™ 7 7840HS": {
+            "clockSpeed": 5.1,
+            "cores": 8,
+            "threads": 16
+        },
+        "Ryzen™ 9 7940HS": {
+            "clockSpeed": 5.2,
+            "cores": 8,
+            "threads": 16
+        },
+        "Ryzen™ AI 7 350": {
+            "clockSpeed": 5,
+            "cores": 8,
+            "threads": 16
+        },
+        "Ryzen™ AI 9 HX 370": {
+            "clockSpeed": 5.1,
+            "cores": 12,
+            "threads": 24
+        }
+    },
+    "minPower": 100,
+    "recomendedPower": 180,
+    "maxPower": 240
+};
+
+const frameworkDesktop: FrameworkDesktop = {
+
+};
+
+export const computers: Computers = {
+    laptops: {
+        framework12: framework12,
+        framework13: framework13,
+        framework16: framework16
+    },
+    desktops: {
+        desktop: frameworkDesktop
+    }
+};
--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,97 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface Ram {
+  "slots": number,
+  "maxTotal": number,
+  "maxPerSlot": number
+};
+
+export interface Storage {
+  "slots": number,
+  "sizes": number[]
+}
+
+export interface CPU {
+  "clockSpeed": number,
+  "cores": number,
+  "threads": number
+};
+
+export interface Fw13Package extends CPU {
+  "battery": number,
+  "webcamGen": number,
+  "displayInfo": string
+}
+
+
+
+export interface Framework12 {
+  "ram": Ram,
+  "storage": Storage,
+  "cpu": {
+    "i3-1315U": CPU,
+    "i5-1334U": CPU
+  },
+  "power": number
+};
+
+export interface Framework13 {
+  "ram": Ram,
+  "storage": Storage,
+  "old builds": {
+    "amd": {
+      "AMD Ryzen™ 5 7640U": Fw13Package,
+      "AMD Ryzen™ 5 7640U (2.8k Display)": Fw13Package
+    }
+  },
+  "new builds": {
+    "cpu": {
+      "amd": {
+        "Ryzen™ AI 5 340": CPU,
+        "Ryzen™ AI 7 350": CPU,
+        "Ryzen™ AI 9 HX 370": CPU
+      },
+      "intel": {
+        "Ultra 5 125H": CPU,
+        "Ultra 7 155H": CPU,
+        "Ultra 7 165H": CPU
+      }
+    },
+    "display": {
+      "2.2K Display": string,
+      "2.8K Display (rounded)": string
+    }
+  },
+  "power": number
+};
+
+export interface Framework16 {
+  "ram": Ram,
+  "storage": Storage,
+  "cpu": {
+    "Ryzen™ 7 7840HS": CPU,
+    "Ryzen™ 9 7940HS": CPU,
+    "Ryzen™ AI 7 350": CPU,
+    "Ryzen™ AI 9 HX 370": CPU
+  },
+  "minPower": number,
+  "recomendedPower": number,
+  "maxPower": number
+};
+
+export interface FrameworkDesktop {
+
+};
+
+export interface Computers {
+  "laptops": {
+    "framework12": Framework12,
+    "framework13": Framework13,
+    "framework16": Framework16
+  },
+  "desktops": {
+    "desktop": FrameworkDesktop
+  }
+};
